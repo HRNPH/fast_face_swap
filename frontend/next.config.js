707ed023e81130a891be6c3226379e4a36307f22
HRNPH <hrnph@protonmail.com>
2024-01-02 22:57:40 +0700

    upload file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        API_URL: process.env.API_URL
+    },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        API_URL: process.env.API_URL
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
